2a16803a2ee0b1cbfee6aa4feb1c12e051c25444
kiko <francescribac@gmail.com>
2022-07-01 20:00:44 +0200

    Created the chart visualisation tool

---
+++ apps/progress_chart.py
@@ -0,0 +1,37 @@
+import json
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+Plotting of the number of attempts per level per user
+"""
+
+def progress_plot(name):
+
+    n = input('Enter 0 for accuracy, 1 for speed or 2 for number of attempts')
+    n = int(n)
+    f = open('data/Historical.json')
+    # returns JSON object as a dictionary
+    data = json.load(f)
+
+
+    objects = ('Level 1', 'Level 2', 'Level 3')
+    y_pos = np.arange(len(objects))
+    performance = [data[name]['Level1'][n],data[name]['Level2'][n],data[name]['Level3'][n]]
+
+    if n == 0:
+        ylabel = 'Accuracy per level'
+    elif n == 1:
+        ylabel = 'Speed per level'
+    elif n == 2:
+        ylabel = 'Attempts per level'
+
+    plt.bar(y_pos, performance, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel(ylabel)
+    plt.title(f"{name}'s progress")
+
+    plt.show()
+
+progress_plot('Giorgio')
